cca7df367fab65650e6746eca8417dbec2f76c8e
TheodoraGeorge <es-Theodora.Riad23@alexu.edu.eg>
2023-05-21 03:11:06 +0300

    history/updated

--- src/screens/HistoryScreen/HistoryScreen.js
+++ src/screens/HistoryScreen/HistoryScreen.js
@@ -1,40 +1,83 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, useWindowDimensions } from 'react-native';
-import product from '../../data/product';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image,ScrollView} from 'react-native';
+import connection from '../../router/connection';
+
+
 const HistoryScreen = () => {
     const [productInfo, setProductInfo] = useState([]);
+    const [showMessage, setShowMessage] = useState(false);
 
-    const Img = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/mouse1.jpg';
+  useEffect(() => {
+    //Get product IDs from history table
+        connection.get('/histories').then((response) => {
+        const productIds = response.data.map((row) => row.product_id);
+        console.log(productIds);
+        //Get product names and links from products table
+        connection.get('/products').then((response) => {
+            const productInfo = response.data.filter(row=> productIds.includes(row.id)).map((row) => ({
+                id: row.id,
+                name: row.name,
+                image: row.img_url,
+            }));
+            // setProductInfo(productInfo);
+            // console.log(productInfo);
+            //Map productInfo to new array
+            const sortedProductInfo = productIds.map((id) =>
+            productInfo.find((info) => info.id === id)
+             );
+            //Sort new array by order of productIds
+            sortedProductInfo.sort((a, b) =>
+            productIds.indexOf(a.id) - productIds.indexOf(b.id)
+            );
+            // Step 3: Do something with the sorted product info
+            console.log(sortedProductInfo);
+            setProductInfo(sortedProductInfo);
+                })
+            .catch((error) => {
+            console.error(error);
+            });
+        })
+        .catch((error) => {
+        console.error(error);
+        });
+        
+    }, []);
+    useEffect(() => {
+        // Step 4: Show "history empty" message after 10 seconds if productInfo is still empty
+        if (productInfo.length === 0) {
+          const timer = setTimeout(() => {
+            setShowMessage(true);
+          }, 1000);
+          return () => clearTimeout(timer);
+        }
+      }, [productInfo]);
+    
+      if (showMessage) {
+        return (
+          <View>
+             <Text style={styles.empty} >No History</Text>
+          </View>
+        );
+      }
     return (
-        <View >
-            {/* <Text style={styles.container}>My History</Text> */}
-            <View style={styles.root}>
-
-                <Image style={styles.image} source={{ uri: Img }} />
-                <View style={styles.rightContainer}>
+        <ScrollView>
+         <View >
+         {productInfo.map((product) => (
+            <View style={styles.root} key={product.id}>
+                    <Image style={styles.image} source={{ uri: product.image }} />
+                    <View style={styles.rightContainer}>
                     <Text style={styles.title} numberOfLines={3}>
-                        {product.title}
+                        {product.name}
                     </Text>
-                    
-                  
-
+                    </View>
                 </View>
-
+            ))}
             </View>
-        </View>
+             </ScrollView>   
     );
 };
+
 const styles = StyleSheet.create({
-    // container: {
-
-    //     marginBottom: 20,
-    //     marginLeft: 130,
-    //     marginTop: 20,
-    //     color:'#FE8113',
-    //     fontSize: 30,
-    //     fontWeight: 'bold',
-
-    // },
     root: {
         flexDirection: 'row',
         borderWidth: 1,
@@ -43,7 +86,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         marginVertical: 5,
         padding:10,
-        marginTop: 20,
+        marginTop: 8,
         marginLeft:10,
         marginRight:10,
     },
@@ -60,6 +103,13 @@ const styles = StyleSheet.create({
         // fontSize: 15,
         // flex: 2,
 
+    },
+    empty:{
+        fontSize: 40,
+        fontWeight: "normal",
+        marginTop: 245,
+        marginLeft:110,
+        marginRight:10,
     },
     rightContainer: {
         flex: 6,
