5ec9c5d7cf3daa42463781c418455145b10047dc
martina mamdouh <martinadouha2000@gmail.com>
2023-05-22 22:19:54 +0300

    displayWishlist

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,5 +1,5 @@
-import React ,{useState,useEffect} from 'react';
-import { View , StyleSheet,TouchableOpacity,Image,Text , Pressable} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, TouchableOpacity, Image, Text, Pressable } from 'react-native';
 import axios from "axios";
 //import AsyncStorage from '@react-native-community/async-storage'
 import products from '../../data/products';
@@ -8,77 +8,89 @@ import WishlistScreen from '../../screens/WishlistScreen';
 import { it } from 'node:test';
 import { faV } from '@fortawesome/free-solid-svg-icons';
 import connection from '../../router/connection';
-const Favorite = ({item}) => {
-    
-    const heartEmpty='https://www.citypng.com/public/uploads/preview/-51610329431xv3s3v3d9v.png';
-    const heartFill='https://www.pngitem.com/pimgs/m/307-3070057_red-heart-outline-png-transparent-png.png';
+const Favorite = ({ item }) => {
+
+    const heartEmpty = 'https://www.citypng.com/public/uploads/preview/-51610329431xv3s3v3d9v.png';
+    const heartFill = 'https://www.pngitem.com/pimgs/m/307-3070057_red-heart-outline-png-transparent-png.png';
     const [favorite, setFavorite] = useState([])
-    const baseURL='http://localhost:3000/api/v1/products/';
+    const baseURL = 'http://localhost:3000/api/v1/products/';
     const params = {
         product_id: item.id
-      }
-    const headers = {
-    'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.7xGXdq5PJHMYyvqCCV0hwA2lCCde9LrUZkyFTX2Bc0s',
-    'Content-Type': 'application/json'
     }
+    const headers = {
+        'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.7xGXdq5PJHMYyvqCCV0hwA2lCCde9LrUZkyFTX2Bc0s',
+        'Content-Type': 'application/json'
+    }
+
+    useEffect(() => {
+        connection.get('/favorites').then(response => {
+            // console.log(response.data);
+            const productIds = response.data.map((row) => row.product_id);
+            console.log(productIds);
+            setFavorite(productIds);
+            console.log(favorite);
+        })
+            .catch(error => { console.error(error); });
+    }, []);
     const toggleWishlist = async () => {
         //console.warn(item.id);
-        
-        if (favorite.includes(item)) {
-        setFavorite(favorite.filter(id => id !== item));
-          console.log("favorite items removed",item)
 
+        if (favorite.includes(item)) {
+            setFavorite(favorite.filter(id => id !== item));
+            console.log("favorite items removed", item)
+            connection.post('/favorites/destroy', params).then(response => { console.log(response.data); })
+                .catch(error => { console.error(error); });
         } else {
-          
-            setFavorite([...favorite , item]);
-            console.log("favorite items added",item);
-            connection.post('/favorites',params).then(response => {console.log(response.data);})
-            .catch(error => {console.error(error);});
-        
-        
-        //   <WishlistScreen item={item}/>
-          
-         }
-        
-      };
+
+            setFavorite([...favorite, item]);
+            console.log("favorite items added", item);
+            connection.post('/favorites', params).then(response => { console.log(response.data); })
+                .catch(error => { console.error(error); });
+
+
+            //   <WishlistScreen item={item}/>
+
+        }
+
+    };
     //   useEffect(() => {
     //     AsyncStorage.setItem('wishlist', JSON.stringify(favorite));
     //   }, [favorite]);
-return(
- 
-    <View style={styles.heartBar}>
-        {/* {ProductItem.map((item)=>( */}
-        
-        <Pressable onPress={toggleWishlist}>
-            <Image
-                style={styles.heartImg}
-                source={
-                    favorite.includes(item) ? { uri: heartFill } : { uri: heartEmpty }
-                }
-                
-            />
-        </Pressable>
-       
-    </View>  
-   
-        );    
-        
-    };
-   
-    const styles = StyleSheet.create({
-        heartBar:{
-            // justifyContent:'center',
-            flexDirection:'row',
-            marginTop:10
-        },
-        heartImg:{
-            width:40,
-            height:40,
-            resizeMode:'cover',
-            marginLeft: 10,
-            marginBottom:10,
-            
-        },
-        
-    });
-export default Favorite ;
+    return (
+
+        <View style={styles.heartBar}>
+            {/* {ProductItem.map((item)=>( */}
+
+            <Pressable onPress={toggleWishlist}>
+                <Image
+                    style={styles.heartImg}
+                    source={
+                        favorite.includes(item) ? { uri: heartFill } : { uri: heartEmpty }
+                    }
+
+                />
+            </Pressable>
+
+        </View>
+
+    );
+
+};
+
+const styles = StyleSheet.create({
+    heartBar: {
+        // justifyContent:'center',
+        flexDirection: 'row',
+        marginTop: 10
+    },
+    heartImg: {
+        width: 40,
+        height: 40,
+        resizeMode: 'cover',
+        marginLeft: 10,
+        marginBottom: 10,
+
+    },
+
+});
+export default Favorite;
--- src/screens/HistoryScreen/HistoryScreen.js
+++ src/screens/HistoryScreen/HistoryScreen.js
@@ -1,27 +1,53 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, useWindowDimensions } from 'react-native';
-import product from '../../data/product';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image,ScrollView} from 'react-native';
+import connection from '../../router/connection';
+
+
 const HistoryScreen = () => {
-
-    const Img = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/mouse1.jpg';
+    const [productInfo, setProductInfo] = useState([]);
+//   Get product IDs from history table
+  useEffect(() => {
+    connection.get('/histories')
+        .then((response) => {
+        const productIds = response.data.map((row) => row.product_id);
+        console.log(productIds);
+        //Get product names and links from products table
+        connection.get('/products')
+            .then((response) => {
+            const productInfo = response.data.filter(row=> productIds.includes(row.id)).map((row) => ({
+                id: row.id,
+                name: row.name,
+                image: row.img_url,
+            }));
+            setProductInfo(productInfo);
+            console.log(productInfo);
+            })
+            .catch((error) => {
+            console.error(error);
+            });
+        })
+        .catch((error) => {
+        console.error(error);
+        });
+        
+}, []);
+    
     return (
-        <View >
-            {/* <Text style={styles.container}>My History</Text> */}
-            <View style={styles.root}>
-
-                <Image style={styles.image} source={{ uri: Img }} />
-                <View style={styles.rightContainer}>
+        <ScrollView>
+         {productInfo.map((product) => (
+            <View style={styles.root} key={product.id}>
+                    <Image style={styles.image} source={{ uri: product.image }} />
+                    <View style={styles.rightContainer}>
                     <Text style={styles.title} numberOfLines={3}>
-                        {product.title}
+                        {product.name}
                     </Text>
-                    
-                  
-
+                    </View>
                 </View>
-
-            </View>
-        </View>
+            ))}
+            </ScrollView>
+        
     );
+
 };
 const styles = StyleSheet.create({
     // container: {
@@ -42,7 +68,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         marginVertical: 5,
         padding:10,
-        marginTop: 20,
+        marginTop: 8,
         marginLeft:10,
         marginRight:10,
     },
