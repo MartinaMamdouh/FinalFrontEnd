9bba30ed360f55148f3040d350db8697d33c5cee
Khaled Hesham <khaledhesham896@gmail.com>
2023-05-28 13:18:47 +0300

    new

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, TouchableOpacity, FlatList, Text, ScrollView } from 'react-native';
+import React, { useState,useEffect ,useCallback} from 'react';
+import { View, StyleSheet, FlatList, Text ,TouchableOpacity,ScrollView} from 'react-native';
 import ProductItem from '../../components/ProductItem';
-import CustomButton from '../../components/CustomButton';
-
 import products from '../../data/products';
 import Favorite from '../../components/Favorite/Favorite';
 import SearchBar from '../../components/SearchBar';
 import { NavigationContainer } from '@react-navigation/native';
 import connection from '../../router/connection';
+import Button from '../../components/Button/Button';
+import { useFocusEffect } from '@react-navigation/native';
 
-const HomeScreen_API = ({ searchValue }: { searchValue: string }) => {
+  
+const HomeScreen_API = ({searchValue}:{searchValue:string}) => {
+
   const [sortBy, setSortBy] = useState(''); 
-  const [term, setTerm] = useState('');
-  const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(1);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
+
+   const [term, setTerm] = useState('');
+   
+   const [products, setProducts] = useState([]);
+   const [page,setPage]=useState(1)
+   
+   const fetchData = useCallback(() => {
+    const fetchSortedData = async () => {
       try {
         const response = await connection.get('/products');
         let sortedProducts = response.data;
-
+  
         if (sortBy === 'price_asc') {
           sortedProducts = sortedProducts.sort((a, b) => a.price - b.price);
         } else if (sortBy === 'price_desc') {
@@ -31,32 +36,56 @@ const HomeScreen_API = ({ searchValue }: { searchValue: string }) => {
         } else if (sortBy === 'rating_desc') {
           sortedProducts = sortedProducts.sort((a, b) => b.rating - a.rating);
         }
-
+  
         setProducts(sortedProducts);
       } catch (error) {
         console.error(error);
       }
     };
-
-    fetchData();
+  
+    connection
+      .get('/products')
+      .then(response => {
+        console.log(response.data);
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(() => {
+        fetchSortedData();
+      });
   }, [sortBy]);
+  
+  useFocusEffect(fetchData);
 
-  const handleSortBy = (sortOption) => {
-    setSortBy(sortOption);
-    setIsDropdownOpen(false);
-  };
 
-  const isSortActive = (sortOption) => {
-    return sortBy === sortOption;
-  };
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+    const handleSortBy = (sortOption) => {
+      setSortBy(sortOption);
+      setIsDropdownOpen(false);
+    };
+  
+    const isSortActive = (sortOption) => {
+      return sortBy === sortOption;
+    };
+  
+    const toggleDropdown = () => {
+      setIsDropdownOpen(!isDropdownOpen);
+    };
+  // console.log(searchValue); 
+   
+   return (
+      <View style={styles.page}>
+        
+         {/* <SearchBar placeholder='Search' value='' /> */}
+         {/* <Text>{term}</Text> */}
+         {/* Render product component */}
+         {/* data is array and flatlist will display components for each item in that array 
+             custom components for each item  */}
 
-  return (
-    <View style={styles.page}>
-      <TouchableOpacity style={styles.dropdownButton} onPress={toggleDropdown}>
+
+<TouchableOpacity style={styles.dropdownButton} onPress={toggleDropdown}>
         <Text style={styles.dropdownButtonText}>Sort By: {sortBy || 'Select an option'}</Text>
         <Text style={styles.dropdownButtonArrow}>{isDropdownOpen ? '▲' : '▼'}</Text>
       </TouchableOpacity>
@@ -88,14 +117,23 @@ const HomeScreen_API = ({ searchValue }: { searchValue: string }) => {
           </TouchableOpacity>
         </ScrollView>
       )}
-      <FlatList
-        data={products}
-        ListEmptyComponent={() => <Text style={{ fontSize: 40, fontWeight: 'bold', color: 'black', textAlign: 'center' }}>No Data</Text>}
-        renderItem={({ item }) => <ProductItem item={item} />}
-        showsVerticalScrollIndicator={false}
-      />
-    </View>
-  );
+
+
+
+
+         <FlatList
+            data={products} ListEmptyComponent={()=><Text style={{fontSize:40,fontWeight:"bold",
+            color:"black",textAlign:'center'}}>No Data</Text>}
+            renderItem={({ item }) => <ProductItem item={item} />}
+            // keyExtractor={({id}) =>id}
+
+            // mafeesh scroll indicator
+            showsVerticalScrollIndicator={false}
+         />
+         {/* <TabNav/> */}
+      </View>
+      
+   );
 };
 
 const styles = StyleSheet.create({
@@ -149,47 +187,39 @@ const styles = StyleSheet.create({
       fontSize: 18,
       fontWeight: 'bold',
    },
-   
-  sortButtonsContainer: {
-   flexDirection: 'row',
-   justifyContent: 'space-around',
-   paddingVertical: 10,
-   borderBottomWidth: 1,
-   borderBottomColor: '#ccc',
- },
- sortButton: {
-   paddingHorizontal: 10,
-   paddingVertical: 5,
-   borderRadius: 5,
-   borderWidth: 1,
-   borderColor: '#ccc',
- },
+   sortButton: {
+     paddingHorizontal: 10,
+     paddingVertical: 5,
+     borderRadius: 5,
+     borderWidth: 1,
+     borderColor: '#ccc',
+   },
  activeSortButton: {
-   backgroundColor: '#ccc',
- },
- sortButtonText: {
-   fontSize: 14,
- },
- 
- dropdownButton: {
-   flexDirection: 'row',
-   alignItems: 'center',
-   justifyContent: 'center',
-   paddingVertical: 10,
-   backgroundColor: '#e0e0e0',
-   marginBottom: 10,
- },
- dropdownButtonText: {
-   fontSize: 16,
-   fontWeight: 'bold',
-   marginRight: 10,
- },
- dropdownButtonArrow: {
-   fontSize: 18,
- },
- dropdownContainer: {
-   backgroundColor: '#e0e0e0',
-   maxHeight: 150,
- },
+  backgroundColor: '#ccc',
+},
+sortButtonText: {
+  fontSize: 14,
+},
+
+dropdownButton: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingVertical: 10,
+  backgroundColor: '#e0e0e0',
+  marginBottom: 10,
+},
+dropdownButtonText: {
+  fontSize: 16,
+  fontWeight: 'bold',
+  marginRight: 10,
+},
+dropdownButtonArrow: {
+  fontSize: 18,
+},
+dropdownContainer: {
+  backgroundColor: '#e0e0e0',
+  maxHeight: 150,
+},
 });
 export default HomeScreen_API; 
