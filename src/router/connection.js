f82740c92367ccf233c60dc9725bf81653334373
martina mamdouh <martinadouha2000@gmail.com>
2023-06-12 22:37:47 +0300

    update

--- src/router/connection.js
+++ src/router/connection.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const token='eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.7xGXdq5PJHMYyvqCCV0hwA2lCCde9LrUZkyFTX2Bc0s';
 const connection = axios.create({
-  baseURL: 'http://192.168.56.1:3000/api/v1',
+  baseURL: 'http://10.0.2.2:3000/api/v1',
   headers:{'Authorization':`Bearer ${token}`}
 });
 
--- src/screens/TouchIDScreen/TouchIDScreen.js
+++ src/screens/TouchIDScreen/TouchIDScreen.js
@@ -6,17 +6,27 @@ const TouchIDScreen = () => {
 
     const isSupported = async () => {
         const supported = await TouchID.isSupported();
-        if (supported) {
-            console.log("supported");
-            // Touch ID is supported, continue with Touch ID authentication
-        }
-        else { // Touch ID is not supported, display an error message 
-            console.log("not supported");
-        }
+        supported.then(biometryType => {
+            // Success code
+            if (biometryType === 'FaceID') {
+                console.log('FaceID is supported.');
+            } else {
+                console.log('TouchID is supported.');
+            }
+        })
+        // if (supported) {
+        //     console.log("supported");
+        //     // Touch ID is supported, continue with Touch ID authentication
+        // }
+        // else { // Touch ID is not supported, display an error message 
+        //     console.log("not supported");
+        // }
 
         try {
-            const result = await TouchID.authenticate();
-            console.log('Fingerprint authentication successful:', result);
+            const result = await TouchID.authenticate('Scan your fingerprint to continue');
+            const fingerprintID = result.fingerprintID;
+            console.log('User fingerprint ID:', fingerprintID);
+            //console.log('Fingerprint authentication successful:', result);
             // Perform any necessary actions after successful authentication
           } catch (error) {
             console.log('Fingerprint authentication failed:', error);
