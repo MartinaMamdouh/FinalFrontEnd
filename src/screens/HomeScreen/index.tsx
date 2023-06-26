512f50030266156041d7d9f4ce704205e71f5f2a
mariettemagdi23 <es-Mariette.Gad2023@alexu.edu.eg>
2023-06-26 03:49:24 +0300

    filterStartup

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { View, StyleSheet, FlatList, Text, ActivityIndicator, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import {Modal, View, StyleSheet, FlatList, Text, ActivityIndicator, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
 import ProductItem from '../../components/ProductItem';
 import connection from '../../router/connection';
 import { useFocusEffect } from '@react-navigation/native';
-import Feather from 'react-native-vector-icons/Feather'; 
-const HomeScreen_API = () => {
+import styles from './styles';
+import Ionicons from 'react-native-vector-icons/Ionicons'; 
+import Drawer from '../../components/Drawer';
 
+const HomeScreen_API = ({navigation}) => {
+
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
    const [currentPage, setCurrentPage] = useState(1);
    const [loading, setLoading] = useState(false);
    const [products, setProducts] = useState([]);
@@ -88,6 +93,11 @@ const HomeScreen_API = () => {
    const toggleDropdown = () => {
       setIsDropdownOpen(!isDropdownOpen);
    };
+   
+ 
+     const toggleDrawer = () => {
+       setIsDrawerOpen(!isDrawerOpen);
+     };
 
    return (
       <View style={styles.page}>
@@ -123,14 +133,14 @@ const HomeScreen_API = () => {
                      <Text style={styles.sortButtonText}>Rating (High to Low)</Text>
                   </TouchableOpacity>
                </ScrollView>
-            )}
+            )} 
             {/* filter  */}
-              <TouchableOpacity style={styles.filterContainer}>
+              <TouchableOpacity style={styles.filterContainer} onPress={() => setIsModalOpen(true)} >
               <Text style={styles.dropdownButtonText}> Filter</Text>
-               <Feather name='filter' color='grey' size={25}/>
-               
+               <Ionicons name='options-sharp' color='grey' size={25}/>
+               {/* {isDrawerOpen &&  <Drawer navigation={navigation} />} */}
                </TouchableOpacity>
-  
+               <Modal visible={isModalOpen} animationType="slide"><Drawer navigation={navigation}/></Modal>
 
             <FlatList
                data={products} ListEmptyComponent={() => <ActivityIndicator size="large" />}
@@ -167,162 +177,4 @@ const HomeScreen_API = () => {
 
    );
 };
-
-const styles = StyleSheet.create({
-   page: {
-      // width:'100%',
-      //flex:1,
-      padding: 10,
-   },
-   pageContent: {
-      // position: 'absolute',
-      zIndex: 0,
-      width: '100%',
-      height: '100%',
-   },
-   root: {
-      flexDirection: 'row',//one row and different columns 
-      borderWidth: 1,
-      borderColor: '#d1d1d1',
-      borderRadius: 10,// to make border sharper
-      backgroundColor: '#fff',
-      width: '100%',
-   },
-   ratingContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginVertical: 5,
-   },
-   starImgStyle: {
-      margin: 2,
-      width: 20,
-      height: 20,
-      resizeMode: 'cover',
-   },
-   image: {
-      flex: 2,
-      height: 150,
-      resizeMode: 'contain',//cover the whole image even the image will not cover the whole page
-      // width:150,
-      // height:150, 
-   },
-   rightContainer: {
-      padding: 10,// blank distance between text and image 
-      // width:'100%',
-      flex: 3,
-   },
-   title: {
-      fontSize: 18,
-      fontWeight: 'bold',
-   },
-   price: {
-      fontSize: 18,
-      fontWeight: 'bold',
-   },
-   loadingcontainer: {
-      flex: 10,
-      justifyContent: 'center',
-   },
-   loadinghorizontal: {
-      flexDirection: 'column',
-      marginHorizontal: 30,
-      padding: 10,
-   },
-
-   //////pagination 
-   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-   },
-   mainData: {
-      flex: 1,
-      padding: 10,
-   },
-   item: {
-      backgroundColor: '#f9c2ff',
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
-   },
-   pagetitle: {
-      fontSize: 16,
-   },
-   pageNumbers: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 5,
-      zIndex: 1,
-   },
-   button: {
-      backgroundColor: '#b2d8d8',
-      padding: 8,
-      marginHorizontal: 145,
-      borderRadius: 10,
-      borderWidth: 1,
-      borderColor: "#008080",
-   },
-   text: {
-      color: "#008080",
-      fontSize: 15,
-   },
-   footer: {
-      // Add any desired styles for the footer, for example:
-      backgroundColor: '#f8f9fa',
-      padding: 10,
-      marginBottom: 10,
-   },
-
-
-   //sort
-   sortButton: {
-      paddingHorizontal: 10,
-      paddingVertical: 5,
-      borderRadius: 5,
-      borderWidth: 1,
-      borderColor: '#ccc',
-   },
-   activeSortButton: {
-      backgroundColor: '#ccc',
-   },
-   sortButtonText: {
-      fontSize: 14,
-   },
-
-   dropdownButton: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 10,
-      backgroundColor: '#e0e0e0',
-      marginBottom: 10,
-      marginRight: 260,
-     
-   },
-   dropdownButtonText: {
-      fontSize: 16,
-      fontWeight: 'bold',
-      marginRight: 10,
-   },
-   dropdownButtonArrow: {
-      fontSize: 18,
-   },
-   dropdownContainer: {
-      backgroundColor: '#e0e0e0',
-      maxHeight: 150,
-   },
-   filterContainer:{          
-      borderBottomColor: 'grey',
-      backgroundColor: '#e0e0e0',
-      position: 'absolute',
-      right: 5,
-      flexDirection: 'row',
-      alignItems: 'center',
-      height: 45,
-      paddingVertical: 5,
-      paddingHorizontal:15,
-      marginBottom:10,
-      
-   },
-});
 export default HomeScreen_API; 
